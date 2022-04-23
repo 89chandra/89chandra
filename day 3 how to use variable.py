f4fa2ed4d6071d0ebc6e529182fee41ff68c438e
89chandra <102866290+89chandra@users.noreply.github.com>
2022-04-23 12:10:05 +0530

    Add files via upload

---
+++ day 3 how to use variable.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+porosity_core = 0.5
+
+
+# In[2]:
+
+
+porosity_core
+
+
+# In[3]:
+
+
+porosity_core*108
+
+
+# In[4]:
+
+
+type(porosity_core)
+
+
+# In[5]:
+
+
+b= 'petroleum'
+
+
+# In[7]:
+
+
+b+ 'engineer'
+
+
+# In[8]:
+
+
+b= petroleum
+
+
+# In[9]:
+
+
+b
+
+
+# In[11]:
+
+
+porosity_core
+
+
+# * == for comparison, = for assiging value to variable
+
+# In[13]:
+
+
+print(2==3)
+
+
+# In[14]:
+
+
+@ = 565
+
+
+# In[15]:
+
+
+_ = 'special'
+
+
+# In[16]:
+
+
+_
+
+
+# In[ ]:
+
+
+
+
